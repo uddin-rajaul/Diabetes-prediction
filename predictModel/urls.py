f83cdfd95e6696aef83f5cdae4c0f12e0c11d177
Rajaul <uddin.rajaul1@gmail.com>
2024-04-28 22:56:49 +0545

    Add url for index and result

--- predictModel/urls.py
+++ predictModel/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('result/', views.result, name='result'),
+    path('results/', views.result, name='result'),
 ]
